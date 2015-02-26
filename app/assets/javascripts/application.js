8b9c8e398c4c8cc8d17a7c4fe919f9381701453f
Jarf <crazymoose7@gmail.com>
2015-02-25 19:47:02 -0500

    Moved arond some javascript

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,19 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+var ready2 = function(){
+
+	$('body').on('focus', 'input[type="date"]', function(){
+		$(this).css('opacity','1');
+	});
+
+	$('body').on('blur', 'input[type="date"]', function(){
+		if($(this).val()===""){
+			$(this).css('opacity','.5');
+		};	
+	});
+}
+
+$(document).ready(ready2);
+$(document).on('page:load', ready2);

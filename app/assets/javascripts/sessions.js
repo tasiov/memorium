609b375965f4eeaa3284c40ec33833e62c5c6ab0
Jarf <crazymoose7@gmail.com>
2015-02-26 21:54:04 -0500

    Made a lot of jquery/css changes to the create a memorial popup. Needed to add a textarea field which required more jquery in order to function like the other input fields. Also added jquery to handle submissions with blank fields (it displays a temporary error message). Also, a Haiku. Obsession begins Time passes much too quickly At least it is done

--- app/assets/javascripts/sessions.js
+++ app/assets/javascripts/sessions.js
@@ -1,8 +1,8 @@
 
 var ready = function(){
 
-	//Defines the "text field in focus" event.
-	$('body').on('focus', 'input[type="text"]', function(){
+	//Defines the "text field/textarea in focus" event.
+	$('body').on('focus', 'input[type="text"],textarea', function(){
 		if($(this).attr('id').indexOf('password')!==-1){ //If the id contains the word "password"...
 			$(this).val(""); // Empty out the field.
 			$(this).attr('type','password'); //Change it into a password_field.
@@ -10,9 +10,11 @@ var ready = function(){
 		};
 
 		//When a key is depressed...
-		$('input[type="text"]').on('keydown', function(){
+		$('input[type="text"],textarea').on('keydown', function(){
 			//If the text field contains the default text...
-			if($(this).val()===$(this).attr('value')){
+			if  (
+					$(this).val() === $(this).attr('value') || ($(this).val() === $(this).text())
+				){
 				$(this).css("opacity", "1"); //change the opacity to 1.
 
 				if($(this).attr('class')!=="edit_field"){ //As long as this isn't an editing field...
@@ -23,15 +25,21 @@ var ready = function(){
 	});
 
 	//Defines the "text field no longer in focus" event.
-	$('body').on('blur', 'input[type="text"],input[type="password"]', function(){
+	$('body').on('blur', 'input[type="text"],input[type="password"],textarea', function(){
 		//If the text field is empty...
 		if($(this).val()===""){
 			if($(this).attr('id').indexOf('password')!==-1){ //If the id contains the word "password"...
 				$(this).attr('type','text'); //...change it back to a text field.
 			};
 
-			//Resets the text field to the default value.
-			value = $(this).attr("value");
+			//Resets the text field/textarea to the default value.
+			if($(this).attr('id')==="memorial_description"){ //Adds special case for textarea.
+				value = $(this).text();
+			}
+			else{//All other cases.
+				value = $(this).attr("value");
+			}
+
 			$(this).val(value);
 
 			//Resets the opacity only if "this" isn't an editing text_field.

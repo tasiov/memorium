47b6cc871ca02d0c7348331ff14d4eccd14afb23
Tasio Victoria <tasiovictoria@gmail.com>
2015-03-01 19:18:42 -0500

    implement search bar dropdown and button features

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,5 +1,6 @@
 var memorial_ready = function() {
 
+	// get the current list of users from the database
  	var getRequest = $.ajax({
 		url: '/users.json',
 		dataType: 'json',
@@ -10,28 +11,77 @@ var memorial_ready = function() {
 	var input = $('#search').val();
 	var userJson = JSON.parse(getRequest["responseText"]);
 	var userNames = [];
+	var dropdown = $('#search-dropdown');
+
+
+  // create an array of user's first + last names
 	for (var i in userJson) {
 		var name = userJson[i].first_name + " " + userJson[i].last_name;
 		userNames.push(name);
 		console.log(name);
 	}
 
-	$('.search_bar').on('keyup', function(){
+  // disable chrome dropdown on select fields
+  $(".search_bar").mousedown(function(e) {
+    if ($(this).find('option').length > 20) {
+        e.preventDefault(); //return false will also work
+    }
+	});
+
+  // search bar with dropdown autofill functionality
+	$('.search_bar').on('keyup', function(event){
+		event.preventDefault();
 		var currentSearchString = $(this).val();
-		// console.log("Current Search String");
-		// console.log(currentSearchString);
-		var nameSubarray = [];
+		dropdown.empty();
+
+		if (currentSearchString === "") {
+			dropdown.hide();
+			return;
+		}
 
 		for(var name in userNames){
-			if((userNames[name].toLowerCase().indexOf(currentSearchString.toLowerCase())===0)&&(currentSearchString!=="")){
+			if (userNames[name].toLowerCase().indexOf(currentSearchString.toLowerCase())===0) {
 				console.log(userNames[name]);
-
+				dropdown.append('<li class="drop-names">' + userNames[name] + '</li>');
 			};
-
+			dropdown.show();
 		};
 
+		if (dropdown.is(':empty')) {
+  		dropdown.hide();
+		}
+	});
+
+	// dynamically create buttons based on search bar results
+	$('#search-dropdown').on("click", ".drop-names", function(event) {
+		var searchText = $('.search_bar').val('');
+		var userName = $(this).text();
+		var buttons = $('.invites');
+		buttons.append('<span class="invite-button">Allow ' + userName + ' to contribute</span>',
+			             '<span class="invite-button">Allow ' + userName + ' to view</span>',
+			             '<button id="cancel-invite">x</button>');
+		buttons.show();
+		dropdown.hide();
+	});
+
+  // cancel button reverts to search bar
+  $('.invites').on("click", "#cancel-invite", function(event) {
+  	event.preventDefault();
+  	var buttons = $(this).parent();
+  	revertToSearch(buttons);
+  });
+
+  function revertToSearch(parent) {
+  	parent.empty();
+  	parent.hide();
+  	$('#search')[0].focus();
+  };
+
+  // button click posts to notifications new
+	$('.invites').on("click", ".invite-button", function(event) {
+		console.log("sdf");
 	});
 };
 
 $(".memorials.show").ready(memorial_ready);
-// $(document).on('page:load', memorial_ready);
+$(".memorials.show").on('page:load', memorial_ready);
--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,4 +1,11 @@
 class NotificationsController < ApplicationController
   def new
+    @notification = Notification.new(notif_params)
+    @notification.save
+  end
+
+  private
+  def notif_params
+    params.require(:notification).permit(:message, :created_at, :user_id, :memorial_id)
   end
 end

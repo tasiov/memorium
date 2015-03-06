82fddb4bab8922c70c90a7dc60c8618a716ae0de
Jarf <crazymoose7@gmail.com>
2015-03-06 18:18:26 -0500

    Merged with Carmen.

--- app/assets/javascripts/memorial.js
+++ app/assets/javascripts/memorial.js
@@ -1,16 +1,13 @@
 var memorial_script = function(){
 
-
-
-
-
-
+	// Expands the comment bar when in focus.
 	$('body').on('focus', '#comment_bar', function(){
 		$(this).css('height', '100px');
 		$(this).css('width', '400px');
 		$('.comment-submission').css('display', 'block');
 	});
 
+	//Shrinks the comment bar if no text had been entered and the user clicks away from the comment bar.
 	$('body').on('click', function(e){
 		if(($(e.target).prop('id')!=='comment_bar')&&($(e.target).prop('id')!=='comment_image')&&($(e.target).prop('id')!=='submit_comment')&&($('#comment_bar').val()==="")){
 			$('#comment_bar').css('height', '20px');
@@ -18,6 +15,16 @@ var memorial_script = function(){
 			$('.comment-submission').css('display', 'none');
 		}
 	});
+
+	$('#memorial-image').on('click', 'empty-memorial-picture', function(){
+
+
+
+
+	});
+
+
+
 };
 
 
